997656892539d5aba587c5a94fcc018861a45203
Chandan.Kumar <chandan.kumar@hmcts.net>
2022-06-26 23:15:37 +0100

    internationalized i18n

--- src/main/java/com/cns/restfulws/helloworld/HelloWorldController.java
+++ src/main/java/com/cns/restfulws/helloworld/HelloWorldController.java
@@ -1,15 +1,30 @@
 package com.cns.restfulws.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
-public class HelloWorldController {
+public class  HelloWorldController {
+
+    @Autowired
+    MessageSource messageSource;
 
     @GetMapping(path = "/hello")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-international")
+    public String helloWorldInternational(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null,
+                "Default Good Morning", LocaleContextHolder.getLocale());
     }
 
     @GetMapping(path = "/hello-bean")
