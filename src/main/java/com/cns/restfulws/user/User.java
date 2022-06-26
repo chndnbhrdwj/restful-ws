2a9ec9e80df387ca8e801e955975b730e6699d69
Chandan.Kumar <chandan.kumar@hmcts.net>
2022-06-26 23:52:25 +0100

    static filtering the json response

--- src/main/java/com/cns/restfulws/user/User.java
+++ src/main/java/com/cns/restfulws/user/User.java
@@ -1,11 +1,14 @@
 package com.cns.restfulws.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
 
+    @JsonIgnore
     private Integer id;
 
     @Size(min = 2, message = "Name should be atleast 2 characters long")
