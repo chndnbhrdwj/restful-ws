41cac3f495e408da96e29d4648cbb6103889c822
Chandan.Kumar <chandan.kumar@hmcts.net>
2022-06-26 22:21:10 +0100

    custom not found exception

---
+++ src/main/java/com/cns/restfulws/user/UserController.java
@@ -0,0 +1,37 @@
+package com.cns.restfulws.user;
+
+import com.cns.restfulws.exceptions.CustomNotFoundException;
+import com.sun.jndi.toolkit.url.Uri;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserDaoService userDaoService;
+
+    @GetMapping(path = "/users")
+    public List<User> getAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userDaoService.findOne(id);
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity createUser(@RequestBody User user) {
+        userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
---
+++ src/main/java/com/cns/restfulws/user/UserDaoService.java
@@ -0,0 +1,44 @@
+package com.cns.restfulws.user;
+
+import com.cns.restfulws.exceptions.CustomNotFoundException;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    private static int userCount = 3;
+
+    static {
+        users.add(new User(1, "Adam", new Date()));
+        users.add(new User(2, "Emma", new Date()));
+        users.add(new User(3, "Erica", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User findOne(int id) {
+        return users.stream()
+                .filter(u -> u.getId() == id).findFirst()
+                .orElseThrow(()->new CustomNotFoundException("id: "+id));
+    }
+
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(++userCount);
+        }
+        users.add(user);
+        return user;
+    }
+}
